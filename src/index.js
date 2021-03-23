f9fbc98c95f35dfd92d6acbe2661bf2568b92a98
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2021-03-23 17:48:31 +1300

    Include API Data for Forecast in JavaScript

--- src/index.js
+++ src/index.js
@@ -62,10 +62,146 @@ function handleSubmit(event){
     search(cityInputElement.value);
 }
 
+function formatHours(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+    hours = `0${hours}`;    
+} 
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+    minutes = `0${minutes}`;
+
+    return `${hours}:${minutes}`;
+}
+}
+
+function formatDays(timestamp){
+    let date = new Date(timestamp);
+    let days = [
+        "Sun",
+        "Mon", 
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+    ]
+    let day = days[date.getDay()];
+
+    return `${day}`;
+}
+
+function displayForecast(response){
+    let forecastElement = document.querySelector("#forecast");
+    
+    forecastElement.innerHTML = `
+    <div class="col-2 day1">
+                        <h3 class="forecast-day">
+                            ${formatDays(response.data.list[0].dt * 1000)}
+                            ${formatHours(response.data.list[0].dt * 1000)}
+                        </h3>
+
+                        <img src="http://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png" alt="sunny" id="day1-icon">
+                        <div class="weather-forecast-temperature">
+                            <strong id="temperature-high">
+                                ${Math.round(response.data.list[0].main.temp_max)}°
+                            </strong>
+
+                            <span id="temperature-low">
+                                ${Math.round(response.data.list[0].main.temp_min)}°
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-2 day2">
+                        <h3 class="forecast-day">
+                            ${formatDays(response.data.list[1].dt * 1000)}
+                            ${formatHours(response.data.list[1].dt * 1000)}
+                        </h3>
+                        <img src="http://openweathermap.org/img/wn/${response.data.list[1].weather[0].icon}@2x.png" alt="sunny" id="day2-icon">
+                        <div class="weather-forecast-temperature">
+                            <strong id="temperature-high">
+                                ${Math.round(response.data.list[1].main.temp_max)}°
+                            </strong>
+
+                            <span id="temperature-low">
+                                ${Math.round(response.data.list[1].main.temp_min)}°
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-2 day3">
+                        <h3 class="forecast-day">
+                            ${formatDays(response.data.list[2].dt * 1000)}
+                            ${formatHours(response.data.list[2].dt * 1000)}
+                        </h3>
+                        <img src="http://openweathermap.org/img/wn/${response.data.list[2].weather[0].icon}@2x.png" alt="sunny" id="day3-icon">
+                        <div class="weather-forecast-temperature">
+                            <strong id="temperature-high">
+                                ${Math.round(response.data.list[2].main.temp_max)}°
+                            </strong>
+
+                            <span id="temperature-low">
+                                ${Math.round(response.data.list[2].main.temp_min)}°
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-2 day4">
+                        <h3 class="forecast-day">
+                            ${formatDays(response.data.list[3].dt * 1000)}
+                            ${formatHours(response.data.list[3].dt * 1000)}
+                        </h3>
+                        <img src="http://openweathermap.org/img/wn/${response.data.list[3].weather[0].icon}@2x.png" alt="sunny" id="day4-icon">
+                        <div class="weather-forecast-temperature">
+                            <strong id="temperature-high">
+                                ${Math.round(response.data.list[3].main.temp_max)}°
+                            </strong>
+
+                            <span id="temperature-low">
+                                ${Math.round(response.data.list[3].main.temp_min)}°
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-2 day5">
+                        <h3 class="forecast-day">
+                            ${formatDays(response.data.list[4].dt * 1000)}
+                            ${formatHours(response.data.list[4].dt * 1000)}
+                        </h3>
+                        <img src="http://openweathermap.org/img/wn/${response.data.list[4].weather[0].icon}@2x.png" alt="sunny" id="day5-icon">
+                        <div class="weather-forecast-temperature">
+                            <strong id="temperature-high">
+                                ${Math.round(response.data.list[4].main.temp_max)}°
+                            </strong>
+
+                            <span id="temperature-low">
+                                ${Math.round(response.data.list[4].main.temp_min)}°
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-2 day6">
+                        <h3 class="forecast-day">
+                            ${formatDays(response.data.list[5].dt * 1000)}
+                            ${formatHours(response.data.list[5].dt * 1000)}
+                        </h3>
+                        <img src="http://openweathermap.org/img/wn/${response.data.list[5].weather[0].icon}@2x.png" alt="sunny" id="day3-icon">
+                        <div class="weather-forecast-temperature">
+                            <strong id="temperature-high">
+                                ${Math.round(response.data.list[5].main.temp_max)}°
+                            </strong>
+
+                            <span id="temperature-low">
+                                ${Math.round(response.data.list[5].main.temp_min)}°
+                            </span>
+                        </div>
+                    </div>`
+}
+
 function search(city){
     let apiKey = "33ab5beffc1f84edf84d559d33e4f095";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
+
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function convertToCelsius(event){
